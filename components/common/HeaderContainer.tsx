3f1491fb93c5f6b6c6e2e30586c5fc4616d4bbe1
Kim taenam <cjb940103@gmail.com>
2021-08-14 23:57:21 +0900

    logout add

--- components/common/HeaderContainer.tsx
+++ components/common/HeaderContainer.tsx
@@ -1,14 +1,24 @@
 import { useRouter } from 'next/router';
 import React from 'react';
-import { useUser } from '../../util';
+import agent from '../../agent';
+import { useUser, fetchJson } from '../../util';
 import Header from './Header';
 
 const HeaderContainer = () => {
     const router = useRouter();
     const { user, mutateUser } = useUser();
 
-    const onLogout = () => {
-        console.log("logout");
+    const onLogout = async() => {
+        try {
+            mutateUser(
+                await fetchJson("/api/logout", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                }),
+            ).then((data) => console.log(data));
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     const onLogin = () => {
